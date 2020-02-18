002fc2783cbab61b167b643abef2e81f202c5ce3
haihuanchen <haihuan.chen@hotmail.com>
2020-02-18 09:29:35 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
@@ -27,31 +43,4 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-  
-def call(env)
-  resp1 = Rack::Response.new
-  req1 = Rack::Rquest.new(nev)
-
-  if req1.path.match(/cart/)
-    @@cart.each do |item|
-      resp1.write "@{item}\n"
-    end
-  elsif req1.path.match(/add/)
-    add_term = req1.params["q"]
-    resp1.write handle_add(add_term)
-  else
-    resp1.write "Path Not Found"
-  end
-
-  resp.finish
-end
-
-def handle_add(add_term)
-  if @@items.include?(add_term)
-    @@cart << add_term
-  else 
-    return "We don't have that item"
-  end
-end
-
 end
